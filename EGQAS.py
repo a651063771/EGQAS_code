946921efd6b9efc2edb20551aab9ed399800a9ea
65106 <651063771@qq.com>
2023-01-18 23:41:45 +0800

    EGQAS_compare

--- EGQAS.py
+++ EGQAS.py
@@ -138,118 +138,118 @@ def compute_normal_com(cluster,partition_weight,partition,result):
     return partition_final,partition_final_index
 score_list=[]
 #data_paths=#[100,101,102,103,104,105,106,107,108,
-data_paths_mitdb = [100,101,102,105,106,107,108,109,111,112,113,114,115,116,117,118,119,121,122,123,124,\
-            200,201,202,203,205,207,208,209,210,212,213,214,215,217,219,220,221,222,223,228,230,231,232,233,234]
-for path in data_paths_mitdb:
-    #record = wfdb.rdrecord('./mgh003/mgh003', physical=False, channels=[3, ])
-    record = wfdb.rdrecord('./mitdb/'+str(path), sampfrom=0,sampto=650000, physical=False, channels=[0, ])
-    #record = wfdb.rdrecord('./wrist/s5_run', physical=False, channels=[4, ])
-    #record = wfdb.rdrecord('./als1/als1', physical=False, channels=[0, ])
-    ventricular_signal = record.d_signal
-    X_train = np.array(ventricular_signal)
-    ventricular_signal_origin=ventricular_signal
-    ventricular_signal= np.reshape(ventricular_signal, [len(ventricular_signal), 1])
-    scaler = StandardScaler().fit(ventricular_signal)
-    #scaler = MinMaxScaler().fit(ventricular_signal)
-    ventricular_signal = scaler.transform(ventricular_signal)  # 转换X
+# data_paths_mitdb = [100,101,102,105,106,107,108,109,111,112,113,114,115,116,117,118,119,121,122,123,124,\
+#             200,201,202,203,205,207,208,209,210,212,213,214,215,217,219,220,221,222,223,228,230,231,232,233,234]
+#for path in data_paths_mitdb:
+#record = wfdb.rdrecord('./mgh003/mgh003', physical=False, channels=[3, ])
+record = wfdb.rdrecord('./mitdb/100', sampfrom=0,sampto=650000, physical=False, channels=[0, ])
+#record = wfdb.rdrecord('./wrist/s5_run', physical=False, channels=[4, ])
+#record = wfdb.rdrecord('./als1/als1', physical=False, channels=[0, ])
+ventricular_signal = record.d_signal
+X_train = np.array(ventricular_signal)
+ventricular_signal_origin=ventricular_signal
+ventricular_signal= np.reshape(ventricular_signal, [len(ventricular_signal), 1])
+scaler = StandardScaler().fit(ventricular_signal)
+#scaler = MinMaxScaler().fit(ventricular_signal)
+ventricular_signal = scaler.transform(ventricular_signal)  # 转换X
 
-    signal_annotation = wfdb.rdann('./mitdb/'+str(path), "atr",summarize_labels=True,sampfrom=0,sampto=100000)
-    if path in [104, 106, 108, 200, 201, 205, 207, 208, 209, 215, 221, 222, 231, 232]:
-        print("filter")
-        ventricular_signal = Kalman1D(ventricular_signal)
-        ventricular_signal = np.reshape(ventricular_signal, [len(ventricular_signal), 1])
-    Pointlists=[]
-    Pointlists_origin=[]
-    for index,value in enumerate(ventricular_signal):
-        Pointlists.append([index*(1/360),value])
-    for index,value in enumerate(ventricular_signal_origin):
-        Pointlists_origin.append([index*(1/360),value])
-    #use DP algorithm to obtain candidate points
-    dp=DPCompress(Pointlists,0.3)
-    points = dp.runDP(Pointlists,0.3)
-    #Using EGQAS algorithm to realize periodic point detection
-    vectors,vectors_index=compute_vector(points)
-    partition,partition_clusters,s_in,s_out,s_m=compute_vector_networkx(vectors)
-    cluster,partition_weight,result=compute_kmeans(partition)
-    partition_final,partition_final_index=compute_normal_com(cluster,partition_weight,partition,result)
-    partition_value=[]
-    vectors_final = []
-    index_list=np.array(Pointlists_origin)[:, 0].tolist()
-    for i in range(len(partition_final)):
+signal_annotation = wfdb.rdann('./mitdb/100', "atr",summarize_labels=True,sampfrom=0,sampto=650000)
+# if path in [104, 106, 108, 200, 201, 205, 207, 208, 209, 215, 221, 222, 231, 232]:
+#     print("filter")
+#     ventricular_signal = Kalman1D(ventricular_signal)
+#     ventricular_signal = np.reshape(ventricular_signal, [len(ventricular_signal), 1])
+Pointlists=[]
+Pointlists_origin=[]
+for index,value in enumerate(ventricular_signal):
+    Pointlists.append([index*(1/360),value])
+for index,value in enumerate(ventricular_signal_origin):
+    Pointlists_origin.append([index*(1/360),value])
+#use DP algorithm to obtain candidate points
+dp=DPCompress(Pointlists,0.3)
+points = dp.runDP(Pointlists,0.3)
+#Using EGQAS algorithm to realize periodic point detection
+vectors,vectors_index=compute_vector(points)
+partition,partition_clusters,s_in,s_out,s_m=compute_vector_networkx(vectors)
+cluster,partition_weight,result=compute_kmeans(partition)
+partition_final,partition_final_index=compute_normal_com(cluster,partition_weight,partition,result)
+partition_value=[]
+vectors_final = []
+index_list=np.array(Pointlists_origin)[:, 0].tolist()
+for i in range(len(partition_final)):
+    for j in partition_final[i]:
+        index=index_list.index(j)
+        partition_value.append([Pointlists_origin[index][0], Pointlists_origin[index][1]])
+        vectors_final.append(vectors[vectors_index.index(j)][1])
+cluster_result_final=[]
+cluster_result_finals=[]
+for i in range(len(partition_final_index)):
+    for _ in range(len(partition[partition_final_index[i]])):
+        cluster_result_finals.append(partition_final_index[i])
+sil_values=sm.silhouette_samples(vectors_final, cluster_result_finals, metric='euclidean')
+sil_score=sm.silhouette_score(vectors_final, cluster_result_finals, metric='euclidean')
+cluster_list=[]
+cluster_values=[]
+cluster_values_mean=[]
+cluster_set=set(cluster_result_finals)
+for i in cluster_set:
+    cluster_values_sub=[]
+    cluster_labels_sub=[]
+    for j in range(len(cluster_result_finals)):
+        if cluster_result_finals[j]== i:
+            cluster_values_sub.append(sil_values[j])
+    cluster_values.append(cluster_values_sub)
+for i in range(len(cluster_values)):
+    cluster_values_mean.append(np.mean(cluster_values[i]))
+
+score_list.append(sm.silhouette_score(vectors_final, cluster_result_finals, metric='euclidean'))
+AMC_value = []
+AMC_index = []
+period_best=[]
+period=[]
+period_std=[]
+for part in partition_final:
+    period_sub=[]
+    part=sorted(part)
+    for i in range(1,len(part)):
+        period_sub.append(part[i]-part[i-1])
+    period_std.append(np.std(period_sub))
+    period.append(period_sub)
+#second-layer modified kmeans
+period_std=np.reshape(period_std,[len(period_std),1])
+std_result=KMeans(n_clusters=2).fit_predict(period_std)
+std_partition=[]
+period_std_cluster_mean=[]
+for i in set(std_result):
+    std_partition_sub=[]
+    period_std_cluster_sub=[]
+    for j in range(len(std_result)):
+        if std_result[j]==i:
+            std_partition_sub.append(partition_final_index[j])
+            period_std_cluster_sub.append(period_std[j])
+    std_partition.append(std_partition_sub)
+    period_std_cluster_mean.append(np.mean(period_std_cluster_sub))
+std_index=period_std_cluster_mean.index(min(period_std_cluster_mean))
+best_std_index=std_partition[std_index]
+cluster_values_mean_final=[]
+for i in best_std_index:
+    AMC_value.append((compute_AMC(partition_weight[i][0], s_in[i], s_out[i], s_m),i))
+# Cluster evaluation after removing outliers and incomplete split points
+vectors_final_sec = []
+index_list = np.array(Pointlists_origin)[:, 0].tolist()
+
+for i in range(len(partition_final)):
+    if partition_final_index[i] in best_std_index:
         for j in partition_final[i]:
-            index=index_list.index(j)
+            index = index_list.index(j)
             partition_value.append([Pointlists_origin[index][0], Pointlists_origin[index][1]])
-            vectors_final.append(vectors[vectors_index.index(j)][1])
-    cluster_result_final=[]
-    cluster_result_finals=[]
-    for i in range(len(partition_final_index)):
+            vectors_final_sec.append(vectors[vectors_index.index(j)][1])
+
+cluster_result_finals_sec = []
+for i in range(len(partition_final_index)):
+    if partition_final_index[i] in best_std_index:
+
         for _ in range(len(partition[partition_final_index[i]])):
-            cluster_result_finals.append(partition_final_index[i])
-    sil_values=sm.silhouette_samples(vectors_final, cluster_result_finals, metric='euclidean')
-    sil_score=sm.silhouette_score(vectors_final, cluster_result_finals, metric='euclidean')
-    cluster_list=[]
-    cluster_values=[]
-    cluster_values_mean=[]
-    cluster_set=set(cluster_result_finals)
-    for i in cluster_set:
-        cluster_values_sub=[]
-        cluster_labels_sub=[]
-        for j in range(len(cluster_result_finals)):
-            if cluster_result_finals[j]== i:
-                cluster_values_sub.append(sil_values[j])
-        cluster_values.append(cluster_values_sub)
-    for i in range(len(cluster_values)):
-        cluster_values_mean.append(np.mean(cluster_values[i]))
-
-    score_list.append(sm.silhouette_score(vectors_final, cluster_result_finals, metric='euclidean'))
-    AMC_value = []
-    AMC_index = []
-    period_best=[]
-    period=[]
-    period_std=[]
-    for part in partition_final:
-        period_sub=[]
-        part=sorted(part)
-        for i in range(1,len(part)):
-            period_sub.append(part[i]-part[i-1])
-        period_std.append(np.std(period_sub))
-        period.append(period_sub)
-    #second-layer modified kmeans
-    period_std=np.reshape(period_std,[len(period_std),1])
-    std_result=KMeans(n_clusters=2).fit_predict(period_std)
-    std_partition=[]
-    period_std_cluster_mean=[]
-    for i in set(std_result):
-        std_partition_sub=[]
-        period_std_cluster_sub=[]
-        for j in range(len(std_result)):
-            if std_result[j]==i:
-                std_partition_sub.append(partition_final_index[j])
-                period_std_cluster_sub.append(period_std[j])
-        std_partition.append(std_partition_sub)
-        period_std_cluster_mean.append(np.mean(period_std_cluster_sub))
-    std_index=period_std_cluster_mean.index(min(period_std_cluster_mean))
-    best_std_index=std_partition[std_index]
-    cluster_values_mean_final=[]
-    for i in best_std_index:
-        AMC_value.append((compute_AMC(partition_weight[i][0], s_in[i], s_out[i], s_m),i))
-    # Cluster evaluation after removing outliers and incomplete split points
-    vectors_final_sec = []
-    index_list = np.array(Pointlists_origin)[:, 0].tolist()
-
-    for i in range(len(partition_final)):
-        if partition_final_index[i] in best_std_index:
-            for j in partition_final[i]:
-                index = index_list.index(j)
-                partition_value.append([Pointlists_origin[index][0], Pointlists_origin[index][1]])
-                vectors_final_sec.append(vectors[vectors_index.index(j)][1])
-
-    cluster_result_finals_sec = []
-    for i in range(len(partition_final_index)):
-        if partition_final_index[i] in best_std_index:
-
-            for _ in range(len(partition[partition_final_index[i]])):
-                cluster_result_finals_sec.append(partition_final_index[i])
-    if len(best_std_index)>1:
-        print("MSV value", sm.silhouette_score(vectors_final_sec, cluster_result_finals_sec, metric='euclidean'))
+            cluster_result_finals_sec.append(partition_final_index[i])
+if len(best_std_index)>1:
+    print("MSV value", sm.silhouette_score(vectors_final_sec, cluster_result_finals_sec, metric='euclidean'))
 
